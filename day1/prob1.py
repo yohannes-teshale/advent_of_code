8208b42ef434756b690bab7f1813bf9ab2d87cba
koeda <yohannesteshle@gmail.com>
2023-12-01 02:03:26 -0600

    second part

--- day1/prob1.py
+++ day1/prob1.py
@@ -1,6 +1,31 @@
+from ast import List
+
+
 sum = 0
 with open("input.txt", "r") as file:
     lines = file.read().split("\n")
+
+def parse_input(lines):
+    alpha_num_dict = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "zero": "0",
+    }
+
+    for i in range(len(lines)):
+        for key, value in alpha_num_dict.items():
+            lines[i] = lines[i].replace(key, value)
+    print(lines)       
+        
+        
+lines= parse_input(lines)
 
 for line in lines:
     i = 0
